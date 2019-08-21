1bfbbf0251906d8e2c0925661218d805b2744a67
seven320 <yosyuaomenw@yahoo.co.jp>
2019-08-22 00:07:22 +0900

    26-C

--- module/tree.py
+++ module/tree.py
@@ -11,11 +11,14 @@ sys.setrecursionlimit(mod) # 再帰回数上限はでdefault1000
 
 # v は今見ている頂点，p はｖの親
 def dfs(node,p): # Depth First Serch
-    # res[node] = color
+    # 終了条件を書く
+    # if graph[node] == xx:
+    # return xxx
+
     for new_node,w in graph[node]:
         if new_node == p:continue
-        if w == 1:dfs(new_node,node)
-        else:dfs(new_node,node)
+        else:
+            return dfs(new_node,node)#深掘りする条件を書く
 #tree 探索　
 
 # node:0から　p:最初はなしなので-1
